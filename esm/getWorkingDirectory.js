ad2c670117962cdab6ecea529ebc64b8fd6af511
沈委 <swzyocowboy@icloud.com>
2019-07-09 14:34:42 +0800

    依赖: :package: 升级依赖

--- esm/getWorkingDirectory.js
+++ esm/getWorkingDirectory.js
@@ -9,12 +9,10 @@ var _isEmpty = _interopRequireDefault(require("./utilities/isEmpty"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var _default = twd => {
-  return {
-    twd: (0, _isEmpty.default)(twd) ? process.cwd() : twd,
-    cwd: process.cwd()
-  };
-};
+var _default = twd => ({
+  twd: (0, _isEmpty.default)(twd) ? process.cwd() : twd,
+  cwd: process.cwd()
+});
 
 exports.default = _default;
 module.exports = exports.default;
