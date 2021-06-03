5b4815974e2b37981f2238859ea7d96588cf6edc
iYoWei <swzyocowboy@icloud.com>
2021-06-03 23:40:51 +0800

    功能: :gear: 更新若干 Git 化名配置

--- src/setGit.js
+++ src/setGit.js
@@ -7,6 +7,12 @@ async function setGit(options = {twd: '.', rules: {}}) {
   const { twd, rules } = merge({
     twd: '.',
     rules: {
+      "alias.ls": "ls-files",
+      "alias.remotes": "remote -v",
+
+      "alias.amend": "!git log -n 1 --pretty=tformat:%s%n%n%b | git commit -F - --amend",
+      "alias.fixup": "commit --fixup=HEAD",
+
       "core.ignorecase": "false",
 
       "alias.s": "status",
@@ -15,24 +21,20 @@ async function setGit(options = {twd: '.', rules: {}}) {
       "alias.cm": "commit -m",
       "alias.tags": "tag -l",
       "alias.co": "checkout",
+      "alias.all": "add -A",
       "alias.pl": "pull --ff-only",
       "alias.stashes": "stash list",
       "alias.med": "merge develop",
       "alias.mem": "merge master",
 
-      "alias.l": "log -16 --color=always --all --topo-order --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative",
+      "alias.l": "log --oneline --color -6",
+      "alias.out": "log @{u}..",
+      "alias.la": "log -16 --color=always --all --topo-order --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative",
       "alias.ll": "log --color=always --all --topo-order --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit",
       "alias.lg": "log -10 --color=always --all --graph --topo-order --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative",
       "alias.lgl": "log --color=always --all --graph --topo-order --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit",
-      "alias.le": "!git log --oneline --color | emojify | less -r",
-      "alias.out": "log @{u}..",
 
       "alias.wdiff": "diff --color-words",
-      "alias.amend": "!git log -n 1 --pretty=tformat:%s%n%n%b | git commit -F - --amend",
-      "alias.fixup": "commit --fixup=HEAD",
-
-      "alias.ls": "ls-files",
-      "alias.remotes": "remote -v",
     },
   }, options);
 
